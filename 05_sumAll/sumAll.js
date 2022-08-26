4cc438d289289808fb352338e324c9e805bb16da
Amy <aljustice25@gmail.com>
2022-08-26 21:04:59 +1200

    Completed exercises 5-7 and testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+    if (typeof one !== 'number' || typeof two !== 'number' || one < 0 || two < 0) {
+        return 'ERROR'
+    } else {
+        if (one > two) {
+            let sum = two;
+            for (i = two; i < one; i++) {
+                sum += two + i
+            }
+            return sum;
+        } else {
+            let sum = one;
+            for (i = one; i < two; i++) {
+                sum += one + i
+            }
+            return sum;
+        }
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  celsius = (temp - 32) * (5/9);
+  if (celsius == 0) {
+    return celsius
+  } else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  fahrenheit = (temp * (9/5)) + 32;
+  if (fahrenheit == 0) {
+    return fahrenheit
+  } else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
