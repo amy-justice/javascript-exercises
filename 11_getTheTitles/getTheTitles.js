0210661582bf932162f0a76d8f1f6ab38d167689
Amy <aljustice25@gmail.com>
2022-08-29 20:10:41 +1200

    exercises 11 & 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+    newArray = [];
+    for (i = 0; i < bookList.length; i++) {
+        newArray.push(bookList[i].title);
+    }
+    
+    return newArray
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest;
+    let ages = [];
+    for (i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath) {
+            let age = people[i].yearOfDeath - people[i].yearOfBirth;
+            ages.push(age);
+        } else {
+            const date = new Date();
+            currentYear = date.getFullYear();
+            let age = currentYear - people[i].yearOfBirth;
+            ages.push(age)
+        }
+    }
+    maxAge = Math.max(...ages);
+    oldestIndex = ages.indexOf(maxAge);
+    oldest = people[oldestIndex]
+    return oldest;
 };
 
 // Do not edit below this line
